46c5e75595dc69f9e928e24428ee5bc45bf46955
Eric Stark <eric.stark@msasafety.com>
2025-03-21 13:21:54 -0400

    showing fixed resolution with automatic integer-scaling of display output

--- sandbox/mockgame.py
+++ sandbox/mockgame.py
@@ -1,20 +1,15 @@
 import pygame
 import os
 
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 240, 320
+WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE | pygame.SCALED)
 pygame.display.set_caption("Mock Game")
 
-SCREEN_BACKGROUND_COLOR = (10, 25, 50)
-
-GAME_FPS = 60
-
-SHIP_WIDTH, SHIP_HEIGHT = 55, 40
-
+SCREEN_BACKGROUND_COLOR = (10, 40, 15)
+SHIP_WIDTH, SHIP_HEIGHT = 16, 16
 YELLOW_SHIP_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_yellow.png"))
 YELLOW_SHIP = pygame.transform.scale(YELLOW_SHIP_IMAGE, (SHIP_WIDTH, SHIP_HEIGHT))
 YELLOW_SHIP = pygame.transform.rotate(YELLOW_SHIP, 90)  # face right
-
 RED_SHIP_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_red.png"))
 RED_SHIP = pygame.transform.scale(RED_SHIP_IMAGE, (SHIP_WIDTH, SHIP_HEIGHT))
 RED_SHIP = pygame.transform.rotate(RED_SHIP, -90)  # face left
@@ -28,19 +23,18 @@ def draw_window(red, yellow):
 
 
 def main():
-    yellow = pygame.Rect(100, HEIGHT//2, SHIP_WIDTH, SHIP_HEIGHT)
-    red = pygame.Rect(WIDTH-100, HEIGHT//2, SHIP_WIDTH, SHIP_HEIGHT)
+    yellow = pygame.Rect(WIDTH//4-SHIP_WIDTH, HEIGHT//2, SHIP_WIDTH, SHIP_HEIGHT)
+    red = pygame.Rect(WIDTH-WIDTH//4, HEIGHT//2, SHIP_WIDTH, SHIP_HEIGHT)
     clock = pygame.time.Clock()
     run = True
+
     VEL = 2
 
     while run:
-        clock.tick(GAME_FPS)
-
+        clock.tick(60) # limit update rate to 60 Hz
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_w]:
             yellow.y -= VEL
@@ -50,7 +44,6 @@ def main():
             red.y -= VEL
         if keys_pressed[pygame.K_DOWN]:
             red.y += VEL
-
         draw_window(red, yellow)
 
     pygame.quit()
