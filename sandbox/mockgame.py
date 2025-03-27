00d1a0e323f2de88042b5414937674e97f56857c
Eric Stark <eric.stark@msasafety.com>
2025-03-27 14:04:09 -0400

    added real pygame sound and mock-up of generalized audio interface

--- sandbox/mockgame.py
+++ sandbox/mockgame.py
@@ -13,16 +13,23 @@ ship_sprite_width, ship_sprite_screen_height = 16, 16
 YELLOW_SHIP_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_yellow.png"))
 YELLOW_SHIP = pygame.transform.scale(YELLOW_SHIP_IMAGE, (ship_sprite_width, ship_sprite_screen_height))
 
+pygame.mixer.init()
+laser_sound = pygame.mixer.Sound(os.path.join("assets", "laserShoot_8bit_22kHz_mono.wav"))
 
-def draw_window (red, yellow):
+def draw_window (yellow):
     WIN.fill(screen_bg_color)
     WIN.blit(YELLOW_SHIP, (yellow.x, yellow.y))
-    WIN.blit(RED_SHIP, (red.x, red.y))
     pygame.display.update()
 
 
 class audio_interface:
+    # Assume a register layout that occurs in one contiguous address range that is twice the size,
+    # in bytes, as the number of interface channels.  The first half of that range assigns a new
+    # volume to the addressed channel and queues the event for processing.  The second half is the
+    # patch assigned to each channel.
     def __init__ (self, num_channels=16, channel_assignments=None, base_address=0x4000):
+        assert(num_channels > 0)
+        assert(base_address >= 0)
         self.num_channels = num_channels
         if channel_assignments is None:
             self.channel_assignments = [i for i in range(num_channels)]
@@ -30,17 +37,22 @@ class audio_interface:
             assert(len(channel_assignments) == num_channels)
             self.channel_assignments = [i for i in channel_assignments]
         self.base_address = base_address
-        # assign initial channel programs
+        # assign initial channel patches
         for channel in range(self.num_channels):
-            self.assign_channel_program(channel, self.channel_assignments[channel])
+            self.assign_channel_patch(channel, self.channel_assignments[channel])
 
-    def assign_channel_program (self, channel, program):
+    def assign_channel_patch (self, channel, patch):
         assert(0 <= channel < self.num_channels)
-        assert(0 <= program < 256)
-        self.channel_assignments[channel] = program
-        self.write_register(self.base_address + self.num_channels + channel, program)
+        assert(0 <= patch < 256)
+        self.channel_assignments[channel] = patch
+        self._write_register(self.base_address + self.num_channels + channel, patch)
 
-    def write_register (address, value):
+    def trigger_channel (self, channel, volume):
+        assert(0 <= channel < self.num_channels)
+        assert(0 <= volume < 256)
+        self._write_register(self.base_address + channel, volume)
+
+    def _write_register (address, value):
         # set [address] = value
         pass
 
@@ -49,8 +61,8 @@ def main (args):
     yellow = pygame.Rect(screen_width//4 - ship_sprite_width, screen_height//2, ship_sprite_width, ship_sprite_screen_height)
     clock = pygame.time.Clock()
     run = True
-
     VEL = 2
+    fire_held = False
 
     while run:
         clock.tick(60) # limit update rate to 60 Hz
@@ -62,11 +74,17 @@ def main (args):
             yellow.y -= VEL
         if keys_pressed[pygame.K_s]:
             yellow.y += VEL
-        if keys_pressed[pygame.K_UP]:
-            red.y -= VEL
-        if keys_pressed[pygame.K_DOWN]:
-            red.y += VEL
-        draw_window(red, yellow)
+        if keys_pressed[pygame.K_a]:
+            yellow.x -= VEL
+        if keys_pressed[pygame.K_d]:
+            yellow.x += VEL
+        if keys_pressed[pygame.K_SPACE]:
+            if not fire_held:
+                fire_held = True
+                pygame.mixer.Sound.play(laser_sound)
+        else:
+            fire_held = False
+        draw_window(yellow)
 
     pygame.quit()
 
